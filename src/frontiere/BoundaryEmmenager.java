dc5cd7c9048f8e8b864a4f9028f78879facfc8e8
Mathis Ravera <rvm3963a@ensfsi.onmicrosoft.com>
2024-03-13 11:58:01 +0100

    verifIdent

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -11,8 +11,7 @@ public class BoundaryEmmenager {
 
 	public void emmenager(String nomVisiteur) {
 		if (controlEmmenager.isHabitant(nomVisiteur)) {
-			System.out.println(
-					"Mais vous êtes déjà un habitant du village !");
+			System.out.println("Mais vous êtes déjà un habitant du village !");
 		} else {
 			StringBuilder question = new StringBuilder();
 			question.append("Êtes-vous :\n");
@@ -27,12 +26,11 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					emmenagerGaulois(nomVisiteur);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +38,28 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		System.out.println("Bienvenue druide" + nomVisiteur);
+		StringBuilder question = new StringBuilder();
+		question.append("Quelle est votre force ?");
+		int forceDruide = Clavier.entrerEntier(question.toString());
+		int effetPotionMin;
+		int effetPotionMax;
+		do {
+			effetPotionMin = Clavier.entrerEntier("Quelle est la potion la plus faible que vous produisez ?");
+			effetPotionMax = Clavier.entrerEntier("Quelle est la potion la plus forte que vous produisez ?");
+			if (effetPotionMin > effetPotionMax) {
+				System.out.println("Attention Druide vous vous �tes tromp� entre le minimum te le maximum");
+			}
+		} while (effetPotionMin > effetPotionMax);
+
+		controlEmmenager.ajouterDruide(nomVisiteur, forceDruide, effetPotionMin, effetPotionMax);
+	}
+	
+	private void emmenagerGaulois(String nomVisiteur) {
+		System.out.println("Bienvenue villageois" + nomVisiteur);
+		StringBuilder question = new StringBuilder();
+		question.append("Quelle est votre force ?");
+		int force = Clavier.entrerEntier(question.toString());
+		controlEmmenager.ajouterGaulois(nomVisiteur, force);
 	}
 }
